c0a256908b423ed9ae4a04fa3519001da965c029
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2021-12-02 12:34:35 -0300

    update

--- experimento-2/tau_funcao.m
+++ experimento-2/tau_funcao.m
@@ -3,6 +3,7 @@ function tau = tau_funcao(steptime,initialvalue,finalvalue,times,values);
     ts = S.SettlingTime;
     values_ss = [];
     j = 1;
+    val_steptime = values(find(times == steptime));
     times = times(find(times > steptime));
     for i=1:length(times)
         time = times(i);
@@ -12,6 +13,6 @@ function tau = tau_funcao(steptime,initialvalue,finalvalue,times,values);
         end
     end
     Vt_ss = sum(values_ss)/length(values_ss);
-    idxs = find(values>=0.63*Vt_ss);
+    idxs = find(values>=0.63*Vt_ss+val_steptime);
     tau = times(idxs(1));
 end
