57d32d47bb7b5408d1094f58edd62f0a60bc5fef
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-01-20 17:25:01 -0300

    experimento-5

---
+++ experimento-5/exp5_script.m
@@ -0,0 +1,38 @@
+%Setup
+clear all;
+close all;
+load('motor.mat');
+h = 1e-3;
+tempo_total = 5;
+h_motor = h*1e-1;
+PO = 0.1;
+ts = 2;
+
+%Estimation of dynamic constants
+zeta = abs(log(PO))/(sqrt(pi^2+log(PO)^2));
+wn = 4/(zeta*ts);
+
+%DC motor parameters
+K = 1.2;
+tau = 0.1077;
+
+%PI control parameters
+Kp = (2*zeta*wn*tau-1)/K;
+Fi = (wn^2*tau)/(Kp*K);
+Ti = 1/Fi;
+
+%Reference
+R_steptime = 1;
+R_initialvalue = 3;
+R_finalvalue = 5;
+
+out = sim('exp5_diagrama.slx',tempo_total/2);
+
+%Plot Vt and R
+plot(out.Vtd.time/h,out.Vtd.signals.values)
+hold on
+plot(out.R.time/h,out.R.signals.values)
+legend('V_t(t_k)','R(t_k)')
+xlabel('k')
+ylabel('Signal')
+title('Discrete time reference and V_t')
