1e7df06f99b6a33de6ba43538b37d7795e1f50b3
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-01-24 23:34:03 -0300

    updates exp5

--- experimento-5/exp5_script.m
+++ experimento-5/exp5_script.m
@@ -1,4 +1,4 @@
-%Setup
+% Setup
 clear all;
 close all;
 load('motor.mat');
@@ -8,27 +8,34 @@ h_motor = h*1e-1;
 PO = 0.1;
 ts = 2;
 
-%Estimation of dynamic constants
+% Estimation of dynamic constants
 zeta = abs(log(PO))/(sqrt(pi^2+log(PO)^2));
 wn = 4/(zeta*ts);
 
-%DC motor parameters
+% DC motor parameters
 K = 1.2;
 tau = 0.1077;
 
-%PI control parameters
+% PI control parameters
 Kp = (2*zeta*wn*tau-1)/K;
 Fi = (wn^2*tau)/(Kp*K);
 Ti = 1/Fi;
 
-%Reference
-R_steptime = 1;
-R_initialvalue = 3;
-R_finalvalue = 5;
+% Reference
+R_steptime = 0;
+R_initialvalue = 5;
+R_finalvalue = 10;
 
+% Initial variables
+x0 = [0;0];
+
+% Deadzone limit
+M = 0;
+
+% Run simulation
 out = sim('exp5_diagrama.slx',tempo_total);
 
-%Plot Vt and R
+% Plot Vt and R
 figure(1)
 plot(out.Vtd.time/h,out.Vtd.signals.values)
 hold on
@@ -39,7 +46,7 @@ ylabel('Signal')
 title('Discrete time reference and V_t')
 saveas(gcf,'imgs/response_ref.png')
 
-%Plot
+% Plot
 figure(2)
 plot(out.Wt.time,out.Wt.signals.values)
 xlabel('t')
