b1d02dcc4111eebd196bc63572e7127902a8366b
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-01-30 18:05:44 -0300

    exp6 v1

--- experimento-6/script_exp6.m
+++ experimento-6/script_exp6.m
@@ -3,7 +3,7 @@ close all;
 load('motor.mat');
 
 % sample times
-h = 0.01;
+h = 1;
 h_motor = 1e-4;
 
 % initial state
@@ -14,12 +14,36 @@ noise_power = 1e-6;
 
 % PRBS settings
 h_PRBS = h;
-gain_PRBS = 5;
+gain_PRBS = 1;
+DC_PRBS = 4;
 
 % motor paramaters
-Kt = 0.15;
-Ka = 8.52;
-Kg = 8.52;
-K = 1.28;
+% Kt = 0.15;
+% Ka = 8.52;
+% Kg = 8.52;
 
+% Simulate 
 out = sim('exp6_diagrama.slx',20);
+
+% Calculate matrices
+Me = [out.Ia.signals.values out.Ue.signals.values];
+Mm = [out.Vt.signals.values out.Um.signals.values];
+
+% Apply OLS
+xe = inv(Me'*Me)*Me'*out.Ia.signals.values;
+xm = inv(Mm'*Mm)*Mm'*out.Vt.signals.values;
+
+% Determine Phi and Gamma
+Phie = xe(1);
+Gammae = xe(2);
+
+Phim = xm(1);
+Gammam = xm(2);
+
+% Estimate other parameters
+Ra = (1-Phie)/Gammae;
+La = -(Ra*h)/(log(Phie));
+f = (1-Phim)/Gammam;
+J = -(f*h)/(log(Phim));
+
+
