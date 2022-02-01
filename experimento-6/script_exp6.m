211ebd7688c5eb6b106a676299ef0cccf23feb94
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-02-01 20:21:24 -0300

    update experimento-5

--- experimento-6/script_exp6.m
+++ experimento-6/script_exp6.m
@@ -3,33 +3,27 @@ close all;
 load('motor.mat');
 
 % sample times
-h = 1;
-h_motor = 1e-4;
+h = 1e-2;
+h_motor = h*1e-3;
 
 % initial state
 x0=[1;0];
 
 % noise
-noise_power = 1e-6;
+noise_power = 0;
 
 % PRBS settings
-h_PRBS = h;
-gain_PRBS = 1;
+gain_PRBS = 2;
 DC_PRBS = 4;
 
-% motor paramaters
-% Kt = 0.15;
-% Ka = 8.52;
-% Kg = 8.52;
-
 % Simulate 
-out = sim('exp6_diagrama.slx',20);
+out = sim('exp6_diagrama.slx',10);
 
 % Calculate matrices
 Me = [out.Ia.signals.values out.Ue.signals.values];
 Mm = [out.Vt.signals.values out.Um.signals.values];
 
-% Apply OLS
+% Apply LS
 xe = inv(Me'*Me)*Me'*out.Ia.signals.values;
 xm = inv(Mm'*Mm)*Mm'*out.Vt.signals.values;
 
@@ -41,9 +35,9 @@ Phim = xm(1);
 Gammam = xm(2);
 
 % Estimate other parameters
-Ra = (1-Phie)/Gammae;
-La = -(Ra*h)/(log(Phie));
-f = (1-Phim)/Gammam;
-J = -(f*h)/(log(Phim));
+Ra_med = (1-Phie)/Gammae;
+La_med = -(Ra*h)/(log(Phie));
+f_med = (1-Phim)/Gammam;
+J_med = -(f*h)/(log(Phim));
 
 
